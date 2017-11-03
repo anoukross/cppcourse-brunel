7f811b607b6963368156a3ca89b8df78ea188e42
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-03 16:00:05 +0100

    Not using it anymore

--- Excitatory.hpp
+++ Excitatory.hpp
@@ -1,21 +1,12 @@
-#ifndef EXCITATORY_HPP
+/*#ifndef EXCITATORY_HPP
 #define EXCITATORY_HPP
 
 #include "Neuron.hpp"
 #include "Network.hpp"
 
-
-/**
- *  The Excitatory class which is a subclass of Neuron
- */
 class Excitatory: public Neuron{	
 	public:
-		
-		/**
-		*  Constructor
-		* calls the constructor of Neuron with index and the amplitude of the postsynaptic current for the excitatory neurons (Je) = 0.1 mV
-        * @param index  the index of the excitaory neuron goes from 0 to nb_excit-1(10000-1)
-        */
+
 		Excitatory(unsigned int index);
 
 	
@@ -23,4 +14,5 @@ class Excitatory: public Neuron{
 };
 
 #endif
+*/
 
