35d72f4d73b73034074892592f3480e5c8119ff7
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-03 15:53:49 +0100

    not using it anymore

--- Excitatory.cpp
+++ Excitatory.cpp
@@ -1,18 +1,11 @@
-#include "Excitatory.hpp"
+/*#include "Excitatory.hpp"
 #include <cassert>
 
-
-/**
-*  Constructor
-*/
 Excitatory::Excitatory(unsigned int index)
-/*! \brief The amplitude of the postsynaptic current of an excitatory neuron = 0.1 mV
-*/
 :Neuron(index, 0.1)
 {
-/*! \brief The index of the excitatory neuron should be between 0 and nb_excit -1 (10000-1)
-*/
+
 assert(index < Network::nb_excit_);  
 assert(index>= 0);
 }
-
+/*
