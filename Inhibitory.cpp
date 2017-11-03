66190e499d920cd08ceb94d582067c41d8bc965c
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-03 15:52:39 +0100

    Not using it anymore

--- Inhibitory.cpp
+++ Inhibitory.cpp
@@ -1,16 +1,9 @@
-#include "Inhibitory.hpp"
+/*#include "Inhibitory.hpp"
 #include <cassert>
 
-/**
-*  Constructor
-*/
 Inhibitory::Inhibitory(unsigned int index)
-/*! \brief The amplitude of the postsynaptic current of an inhibitory neuron = 0.5 mV
-*/
 :Neuron(index, 0.5)
 {
-/*! \brief The index of the inhibitory neuron should be between nb_excit(10000) to nb_neurons -1 (12500-1)
-*/
 assert(index > Network::nb_excit_-1);  
 assert(index < Network::nb_neurons_);
-}
+}*/
