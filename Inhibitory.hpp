2d76e859ad13f1c52bde6dba123e4ab67abebdbb
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-03 15:53:11 +0100

    Not using it anymore

--- Inhibitory.hpp
+++ Inhibitory.hpp
@@ -1,24 +1,14 @@
-#ifndef INHIBITORY_HPP
+/*#ifndef INHIBITORY_HPP
 #define INHIBITORY_HPP
 
 #include "Neuron.hpp"
 #include "Network.hpp"
-
-/**
- *  The Inhibitory class which is a subclass of Neuron
- */
 class Inhibitory: public Neuron{	
 	public:
-	
-		/**
-		*  Constructor
-		* calls the constructor of Neuron with index and the amplitude of the postsynaptic current for the inhibitory neurons (Ji) = 0.5 mV
-        * @param index  the index of the inhibitory neuron goes from nb_excit(10000) to nb_neurons -1 (12500-1)
-        */
 		Inhibitory(unsigned int index);
 	
 	
 };
 
-#endif
+#endif*/
 
