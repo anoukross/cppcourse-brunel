f307baaf8e32e246c5658d5ed45b7552c192e23d
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-05 14:14:21 +0100

    Add googletest

---
+++ googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anoukrossier/Documents/EPFL/BA3/prog/Neuronint/googletest
+# Build directory: /Users/anoukrossier/Documents/EPFL/BA3/prog/Neuronint/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
