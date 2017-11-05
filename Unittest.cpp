c4359d4f97c4032580820ef9b59754ec3ecb5151
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-05 12:58:25 +0100

    Comments tests

--- Unittest.cpp
+++ Unittest.cpp
@@ -6,15 +6,17 @@
 
 
 
-TEST (NetworkTest, TargetsNumber){
+TEST (NetworkTest, ConnexionNumber){
+	//Verification of the number of connexion that one neuron receives
 	Network n;
 	unsigned int nb_connexions(Network::nb_connex_);
 	for(unsigned int t(0); t<Network::nb_neurons_;  ++t){ 
-		EXPECT_EQ(n.getConnexions(t).size(), nb_connexions);
-	}
+		EXPECT_EQ(n.getConnexions(t).size(), nb_connexions); 
+	} 
 }
 
 TEST (NetworkTest, weights){
+	//Verificaiton that the tab Network::weight_ contains je_ for the first 10000 and ji_=-g_*je_ from the laster 2500
 	Network n;
 	double weight(Neuron::je_);
 	for(unsigned int t(0); t<Network::nb_excit_;  ++t){ 
@@ -27,6 +29,7 @@ TEST (NetworkTest, weights){
 }
 
 TEST (NetworkTest, sendSpikes){
+	//Verification of the modification of the buffer of a neuron when one of his connexion has spikes
 	Network n;
 	double weight(Neuron::je_);
 	n.sendSpikes(1,weight);
@@ -41,13 +44,14 @@ TEST (NeuronTest, ConstructorGetters){
 	EXPECT_EQ(n1.getSpikesNumber(), 0);
 	EXPECT_EQ(n1.getClock(), 0);
 	
+	//Before any update, a neuron has not spikes
 	std::vector<unsigned int> spikesTime(n1.getSpikesTime());
 	for(unsigned int i(0);i<n1.getSpikesTime().size();++i){
 		EXPECT_EQ(spikesTime[i],0);
 		EXPECT_EQ(n1.getIncomingSpikes()[i],0);
 	}
 	
-	Neuron n2(2);
+	Neuron n2(2); //Initialisation of neuron of index 2 and membrane potential by default is 0.0
 	EXPECT_EQ(n2.getPotential(), 0.0);
 	EXPECT_EQ(n2.getIndex(), 2);
 	
@@ -59,11 +63,19 @@ TEST (NeuronTest, Spike){
 	EXPECT_EQ(n1.isRefractory(),false);
 	n1.setPotential(25.4);
 	EXPECT_EQ(n1.getPotential(), 25.4);
+	//25.4 is greater than the threshold value ->n1 has spiked
 	EXPECT_EQ(n1.update(0,1),true);
 	EXPECT_EQ(n1.getSpikesNumber(),1);
 	EXPECT_EQ(n1.isRefractory(),true);	
 	n1.update(0,2);
 	EXPECT_EQ(n1.getPotential(),0);
+	//Verification that the refractory period last 2 ms -> 20 step of time
+	for(unsigned int i(3); i<21; ++i){
+		n1.update(0,i);
+		EXPECT_EQ(n1.isRefractory(),true);	
+	}
+	n1.update(0,21);
+	EXPECT_EQ(n1.isRefractory(),false);	
 }
 
 TEST (NeuronTest, Recieve){
@@ -73,6 +85,6 @@ TEST (NeuronTest, Recieve){
 	n1.setPotential(21);
 	double weight(Neuron::je_);
 	n2.receiveSpikes(weight);
-	EXPECT_EQ(n2.getIncomingSpikes()[15],weight); //15 derniere case du buffer car t=1 15%1=15
+	EXPECT_EQ(n2.getIncomingSpikes()[15],weight); //15 last compartment of the buffer because t=1 15%1=15
 	
 }
