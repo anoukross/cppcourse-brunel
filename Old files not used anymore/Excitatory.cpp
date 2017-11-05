3bc298c35cdb33e43c9c867b2806293c0991693a
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-05 16:57:00 +0100

    In case you want to see previous uploads

---
+++ Old files not used anymore/Excitatory.cpp
@@ -0,0 +1,12 @@
+//NOT USED ANYMORE
+
+/*#include "Excitatory.hpp"
+#include <cassert>
+
+Excitatory::Excitatory(unsigned int index)
+:Neuron(index, 0.1)
+{
+assert(index < Network::nb_excit_);  
+assert(index>= 0);
+}
+*/
