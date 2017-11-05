533e641a88e17de670b3f2fee4829baa189dbe9b
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-05 14:43:30 +0100

    doxygen

---
+++ doxygen/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anoukrossier/Documents/EPFL/BA3/Prog/Neuronint/doxygen
+# Build directory: /Users/anoukrossier/Documents/EPFL/BA3/Prog/Neuronint/doxygen
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("libmd5")
+subdirs("qtools")
+subdirs("vhdlparser")
+subdirs("src")
+subdirs("addon")
+subdirs("testing")
