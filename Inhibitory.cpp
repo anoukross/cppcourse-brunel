2d3f47fc64b0c627aca725fc76c7fdb933b333cd
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-05 16:52:33 +0100

    Delete Inhibitory.cpp

--- Inhibitory.cpp
+++
@@ -1,9 +0,0 @@
-/*#include "Inhibitory.hpp"
-#include <cassert>
-
-Inhibitory::Inhibitory(unsigned int index)
-:Neuron(index, 0.5)
-{
-assert(index > Network::nb_excit_-1);  
-assert(index < Network::nb_neurons_);
-}*/
